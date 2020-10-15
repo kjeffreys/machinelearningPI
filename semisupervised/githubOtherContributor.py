9ac8dc345958c28ce991db3350d54b48d9620546
kjeffreys <kajeffr1@asu.edu>
2020-10-15 10:39:12 -0400

    githubOtherContributor.py added main function

---
+++ semisupervised/githubOtherContributor.py
@@ -0,0 +1,12 @@
+'''
+Oh noes! Another user has started contributing (not really).
+
+Now we need to make sure to use proper git in order to merge changes rather than overwrite.
+
+Creating this file as a file that was not locally on my machine. Then creating local change on machine.
+
+Testing that there is successful update when using git add/commit, then git pull, then git push after resolving any conflicts
+'''
+
+if __name__ == "__main__":
+    print('success!')
